469d10f27fcbe35367565a328041b51d57bb3350
118041Mason <41351529+118041Mason@users.noreply.github.com>
2018-07-30 12:17:08 +1000

    Update Form1.vb

--- GROUP PROJECT/LifestyleRPG/LifestyleRPG/Form1.vb
+++ GROUP PROJECT/LifestyleRPG/LifestyleRPG/Form1.vb
@@ -1,13 +1,9 @@
-﻿Public Class Form1
+Public Class Form1
     Dim exp As Integer
     Dim level As Integer = 1
     Dim coins As Integer = 0
     Private Sub btnFinish_Click(sender As Object, e As EventArgs) Handles btnFinish.Click
-
-    End Sub
-
-    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
-        exp = exp + 50
+        exp = 50
         pgbExperience.Value = pgbExperience.Value + 50
         lblExpCount.Text = "Exp " & pgbExperience.Value & "/" & pgbExperience.Maximum
 
@@ -25,6 +21,11 @@
         End If
     End Sub
 
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+
+
+    End Sub
+
     Private Sub btnShop_Click(sender As Object, e As EventArgs) Handles btnShop.Click
         If btnShop.Text = "Open Shop" Then
             Me.Size = New Size(552, 550)
@@ -40,22 +41,50 @@
 
     Private Sub btnDefaultbg_Click(sender As Object, e As EventArgs) Handles btnDefaultbg.Click
         Me.BackColor = SystemColors.Control
+        If Me.ForeColor = Color.FromArgb(255, 255, 255) Then
+            Me.ForeColor = Color.FromArgb(0, 0, 0)
+            Me.lblCoinCounter.ForeColor = Color.FromArgb(0, 0, 0)
+            Me.lblExpCount.ForeColor = Color.FromArgb(0, 0, 0)
+        End If
     End Sub
 
     Private Sub btnBluebg_Click(sender As Object, e As EventArgs) Handles btnBluebg.Click
         Me.BackColor = Color.FromArgb(100, 150, 255)
+        If Me.ForeColor = Color.FromArgb(255, 255, 255) Then
+            Me.ForeColor = Color.FromArgb(0, 0, 0)
+            Me.lblCoinCounter.ForeColor = Color.FromArgb(0, 0, 0)
+            Me.lblExpCount.ForeColor = Color.FromArgb(0, 0, 0)
+        End If
+
     End Sub
 
     Private Sub btnGreenbg_Click(sender As Object, e As EventArgs) Handles btnGreenbg.Click
         Me.BackColor = Color.FromArgb(100, 255, 100)
+        If Me.ForeColor = Color.FromArgb(255, 255, 255) Then
+            Me.ForeColor = Color.FromArgb(0, 0, 0)
+            Me.lblCoinCounter.ForeColor = Color.FromArgb(0, 0, 0)
+            Me.lblExpCount.ForeColor = Color.FromArgb(0, 0, 0)
+        End If
+
     End Sub
 
     Private Sub btnYellowbg_Click(sender As Object, e As EventArgs) Handles btnYellowbg.Click
         Me.BackColor = Color.FromArgb(255, 255, 0)
+        If Me.ForeColor = Color.FromArgb(255, 255, 255) Then
+            Me.ForeColor = Color.FromArgb(0, 0, 0)
+            Me.lblCoinCounter.ForeColor = Color.FromArgb(0, 0, 0)
+            Me.lblExpCount.ForeColor = Color.FromArgb(0, 0, 0)
+        End If
+
     End Sub
 
     Private Sub btnOrangebg_Click(sender As Object, e As EventArgs) Handles btnOrangebg.Click
         Me.BackColor = Color.FromArgb(255, 150, 20)
+        If Me.ForeColor = Color.FromArgb(255, 255, 255) Then
+            Me.ForeColor = Color.FromArgb(0, 0, 0)
+            Me.lblCoinCounter.ForeColor = Color.FromArgb(0, 0, 0)
+            Me.lblExpCount.ForeColor = Color.FromArgb(0, 0, 0)
+        End If
     End Sub
 
     Private Sub btnBlackbg_Click(sender As Object, e As EventArgs) Handles btnBlackbg.Click
@@ -63,5 +92,11 @@
         Me.ForeColor = Color.FromArgb(255, 255, 255)
         Me.lblCoinCounter.ForeColor = Color.FromArgb(255, 255, 255)
         Me.lblExpCount.ForeColor = Color.FromArgb(255, 255, 255)
+
+    End Sub
+
+    Private Sub btnWhitebg_Click(sender As Object, e As EventArgs) Handles btnWhitebg.Click
+        Me.BackColor = Color.FromArgb(255, 255, 255)
     End Sub
 End Class
+
